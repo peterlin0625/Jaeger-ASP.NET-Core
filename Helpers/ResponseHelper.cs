749362e1b17d09ec3764b174c7d34cb12b87f55e
Yowko Tsai <yowko.tsai@skylinetw.com>
2019-04-14 15:34:34 +0800

    Add span in class library

--- Helpers/ResponseHelper.cs
+++ Helpers/ResponseHelper.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using OpenTracing;
 
 namespace Helpers
 {
     public class ResponseHelper
     {
+        private readonly ITracer _tracer;
+
+        public ResponseHelper(ITracer tracer)
+        {
+            _tracer = tracer;
+        }
+
         public string GetResponse(string[] values)
         {
-            return $"return \"{string.Join(',', values)}\" @ {DateTimeOffset.UtcNow}";
+            using (IScope scope = _tracer.BuildSpan(GetType().Name).StartActive(finishSpanOnDispose: true))
+            {
+                string output = $"return \"{string.Join(',', values)}\" @ {DateTimeOffset.UtcNow}";
+                scope.Span.SetTag("module", $"{GetType().Name}:{System.Reflection.MethodBase.GetCurrentMethod().Name}");
+
+                scope.Span.Log(new Dictionary<string, object>
+                {
+                    ["module"] = GetType().Name,
+                    ["function"] = System.Reflection.MethodBase.GetCurrentMethod().Name,
+                    ["eventTime"] = DateTime.Now,
+                    ["input"] = values,
+                    ["output"] = output
+                });
+
+                return output;
+            }
         }
     }
 }
